3c705621b064bcb80221eca8f3fa0f8b0bedad3c
Leon4rdoMonteiro <leonardomonteiro.sh@gmail.com>
2020-08-19 09:58:58 -0300

    project-base

---
+++ src/server.ts
@@ -0,0 +1,15 @@
+import 'reflect-metadata';
+import express from 'express';
+import routes from './routes';
+
+import './database';
+
+const app = express();
+
+app.use(express.json());
+
+app.use(routes);
+
+app.listen(5555, () => {
+    console.log('🚀 Server running on port 5555');
+});
