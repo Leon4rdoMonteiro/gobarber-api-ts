6790d07613d2c857b52558fd4b11a36bd9ebc39c
Leon4rdoMonteiro <leonardomonteiro.sh@gmail.com>
2020-08-26 01:03:51 -0300

    feature:error-handling

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,9 @@
 import 'reflect-metadata';
-import express from 'express';
+import express, { Request, Response, NextFunction } from 'express';
+import 'express-async-errors';
+
+import AppError from './errors/AppError';
+
 import routes from './routes';
 
 import uploadConfig from './config/upload';
@@ -13,6 +17,22 @@ app.use(express.json());
 app.use('/files', express.static(uploadConfig.directory));
 
 app.use(routes);
+
+app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
+    if (err instanceof AppError) {
+        return response.status(err.statusCode).json({
+            status: 'error',
+            message: err.message,
+        });
+    }
+
+    console.error(err);
+
+    return response.status(500).json({
+        status: 'error',
+        message: 'Internal server error',
+    });
+});
 
 app.listen(5555, () => {
     console.log('🚀 Server running on port 5555');
