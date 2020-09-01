63c77a2581a5d790010506af7be882e5ac613724
Leon4rdoMonteiro <leonardomonteiro.sh@gmail.com>
2020-09-01 13:05:33 -0300

    password-recovery

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -11,6 +11,10 @@ import IUsersRepository from '@modules/users/repositories/IUsersRepository';
 
 import UsersRepository from '@modules/users/infra/typeorm/repositories/UsersRepository';
 
+// import IUserTokensRepository from '@modules/users/repositories/IUserTokensRepository';
+
+// import UserTokensRepository from '@modules/users/infra/typeorm/repositories/UserTokensRepository';
+
 container.registerSingleton<IAppointmentsRepository>(
     'AppointmentsRepository',
     AppointmentsRepository,
@@ -20,3 +24,5 @@ container.registerSingleton<IUsersRepository>(
     'UsersRepository',
     UsersRepository,
 );
+
+// container.registerSingleton<IUserTokensRepository>('UserTokensRepository');
