476ec1143afe04f5d19a24f44dfdba49a4ab7c30
Leon4rdoMonteiro <leonardomonteiro.sh@gmail.com>
2020-09-01 15:39:53 -0300

    reset-password

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -11,9 +11,9 @@ import IUsersRepository from '@modules/users/repositories/IUsersRepository';
 
 import UsersRepository from '@modules/users/infra/typeorm/repositories/UsersRepository';
 
-// import IUserTokensRepository from '@modules/users/repositories/IUserTokensRepository';
+import IUserTokensRepository from '@modules/users/repositories/IUserTokensRepository';
 
-// import UserTokensRepository from '@modules/users/infra/typeorm/repositories/UserTokensRepository';
+import UserTokensRepository from '@modules/users/infra/typeorm/repositories/UserTokensRepository';
 
 container.registerSingleton<IAppointmentsRepository>(
     'AppointmentsRepository',
@@ -25,4 +25,7 @@ container.registerSingleton<IUsersRepository>(
     UsersRepository,
 );
 
-// container.registerSingleton<IUserTokensRepository>('UserTokensRepository');
+container.registerSingleton<IUserTokensRepository>(
+    'UserTokensRepository',
+    UserTokensRepository,
+);
